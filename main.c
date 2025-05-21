16053e3dd8c50d94ffbe40f3b341cd5edf8c8584
Francisco Cejas <francejas98@gmail.com>
2025-05-21 18:35:17 -0300

    s

--- main.c
+++ main.c
@@ -6,7 +6,6 @@
 #define FILAS 3
 #define COLUMNAS 3
 
-
 void inicializarTablero(char tablero[FILAS][COLUMNAS]);
 int mostrarMenu();
 void seleccionPiezas(char *p1, char *p2);
@@ -14,7 +13,12 @@ void jugarPvP(char tablero[FILAS][COLUMNAS], char p1, char p2);
 void mostrarTablero(char tablero[FILAS][COLUMNAS]);
 int movimientoInvalido(char tablero[FILAS][COLUMNAS], int fila, int columna);
 int hayGanador(char tablero[FILAS][COLUMNAS], char jugador);
-
+int empate(char tablero[FILAS][COLUMNAS]);
+int modoDificultad();
+void modoFacil();
+void modoMedio();
+void modoDificil();
+int deseaSeguirJugando();
 
 int main()
 {
@@ -34,22 +38,26 @@ int main()
         }
         else
         {
-            int dificultad=modoDificultad();
-            if(dificultad==1)
+            int dificultad = modoDificultad();
+            if (dificultad == 1)
             {
                 modoFacil();
             }
-            else if(dificultad==2)
+            else if (dificultad == 2)
             {
                 modoMedio();
             }
             else
             {
                 modoDificil();
-
             }
         }
 
+        if (!deseaSeguirJugando())
+        {
+            printf("Gracias por jugar.\n");
+            break;
+        }
     }
 
     return 0;
@@ -100,7 +108,6 @@ int modoDificultad()
 
     system("cls");
     return dificultad;
-
 }
 
 void seleccionPiezas(char *p1, char *p2)
@@ -109,29 +116,13 @@ void seleccionPiezas(char *p1, char *p2)
     {
         printf("Jugador 1, elija su ficha (X o O): ");
         scanf(" %c", p1);
-        //evitar minusculas
         *p1 = toupper(*p1);
         if (*p1 != 'X' && *p1 != 'O')
             printf("Ficha invalida. Intente de nuevo.\n");
     }
     while (*p1 != 'X' && *p1 != 'O');
 
-    //Si *p1 es 'X', entonces *p2 será 'O'. Si no, entonces *p2 será 'X'.
-    //condición ? valor_si_verdadero : valor_si_falso;
-
     *p2 = (*p1 == 'X') ? 'O' : 'X';
-
-    /*
-    Es lo mismo que escribir:
-    if (*p1 == 'X')
-    {
-        *p2 = 'O';
-    }
-    else
-    {
-        *p2 = 'X';
-    }
-    */
     printf("Jugador 1: %c | Jugador 2: %c\n", *p1, *p2);
 }
 
@@ -154,16 +145,11 @@ void mostrarTablero(char tablero[FILAS][COLUMNAS])
 void jugarPvP(char tablero[FILAS][COLUMNAS], char p1, char p2)
 {
     int turno = 0, fila, columna;
-    //jugador actual
     char actual;
 
     while (1)
     {
         mostrarTablero(tablero);
-
-        // turno % 2 siempre devuelve 0 o 1, si devuelve 0 le toca a p1, sino a p2.
-        // turno % 2 es igual a 0 ? si si p1 sino p2
-
         actual = (turno % 2 == 0) ? p1 : p2;
         printf("Turno del Jugador %d (%c)\n", (turno % 2) + 1, actual);
 
@@ -173,27 +159,24 @@ void jugarPvP(char tablero[FILAS][COLUMNAS], char p1, char p2)
             scanf("%d", &fila);
             printf("Ingrese columna (0-2): ");
             scanf("%d", &columna);
-            //validacion de casillero libre y que no este fuera de los limites
             if (movimientoInvalido(tablero, fila, columna))
                 printf("Movimiento invalido. Intente de nuevo.\n");
         }
-        while (!movimientoInvalido(tablero, fila, columna));
+        while (movimientoInvalido(tablero, fila, columna));
 
         tablero[fila][columna] = actual;
 
-        //valida ganador
         if (hayGanador(tablero, actual))
         {
             mostrarTablero(tablero);
-            printf("¡Jugador %d (%c) gana!\n", (turno % 2) + 1, actual);
+            printf("\u00a1Jugador %d (%c) gana!\n", (turno % 2) + 1, actual);
             break;
         }
 
-        //evaluacion de empate
         if (empate(tablero))
         {
             mostrarTablero(tablero);
-            printf("¡Empate!\n");
+            printf("\u00a1Empate!\n");
             break;
         }
 
@@ -203,47 +186,71 @@ void jugarPvP(char tablero[FILAS][COLUMNAS], char p1, char p2)
 
 int movimientoInvalido(char tablero[FILAS][COLUMNAS], int fila, int columna)
 {
-    //aca deveria de retornar 1 si es invalido
+    int flag = 1;
+    if (fila >= 0 && fila < FILAS && columna >= 0 && columna < COLUMNAS && tablero[fila][columna] == ' ')
+    {
+        flag = 0;
+    }
+    return flag;
 }
 
 int hayGanador(char tablero[FILAS][COLUMNAS], char jugador)
 {
-    int flag=0;
-    // Filas y columnas
+    int flag = 0;
     for (int i = 0; i < FILAS; i++)
     {
         if (tablero[i][0] == jugador && tablero[i][1] == jugador && tablero[i][2] == jugador)
-            flag=1;
+            flag = 1;
 
         if (tablero[0][i] == jugador && tablero[1][i] == jugador && tablero[2][i] == jugador)
-            flag=1;
+            flag = 1;
     }
 
-    // Diagonales
     if (tablero[0][0] == jugador && tablero[1][1] == jugador && tablero[2][2] == jugador)
-        flag=1;
+        flag = 1;
 
     if (tablero[0][2] == jugador && tablero[1][1] == jugador && tablero[2][0] == jugador)
-        flag=1;
+        flag = 1;
 
     return flag;
 }
 
-
-int empate()
+int empate(char tablero[FILAS][COLUMNAS])
 {
+    int flag = 1;
+    for (int i = 0; i < FILAS; i++)
+        for (int j = 0; j < COLUMNAS; j++)
+            if (tablero[i][j] == ' ')
+                flag = 0;
+    return flag;
 }
 
 void modoFacil()
 {
     printf("Modo fácil seleccionado\n");
 }
+
 void modoMedio()
 {
     printf("Modo medio seleccionado\n");
 }
+
 void modoDificil()
 {
     printf("Modo difícil seleccionado\n");
 }
 
+int deseaSeguirJugando()
+{
+    char respuesta;
+    do
+    {
+        printf("\n¿Desea jugar otra partida? (s/n): ");
+        scanf(" %c", &respuesta);
+        respuesta = tolower(respuesta);
+        if (respuesta != 's' && respuesta != 'n')
+            printf("Respuesta invalida. Ingrese 's' para sí o 'n' para no.\n");
+    } while (respuesta != 's' && respuesta != 'n');
+
+    return (respuesta == 's');
+}
